16cc63f0e69955845608655c57dcbc458a901f63
pixtelation <abhishekmehta50@gmail.com>
2024-03-28 10:43:35 +0530

    New

--- src/test/java/LineHaul/DispatchForm.java
+++ src/test/java/LineHaul/DispatchForm.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 
 public class DispatchForm {
 	
@@ -27,15 +30,15 @@ public class DispatchForm {
 @BeforeSuite	
 	public void OpenBrowser() 
 	{
-//	ChromeOptions options = new ChromeOptions();
-//	options.addArguments("--headless");
+	// ChromeOptions options = new ChromeOptions();
+	// options.addArguments("--headless");
 	
-		// WebDriverManager.safaridriver().setup();
-		// driver = new SafariDriver();	
+		WebDriverManager.safaridriver().setup();
+		driver = new SafariDriver();	
 
 
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		// WebDriverManager.chromedriver().setup();
+		// driver = new ChromeDriver();
 
 
 		driver.manage().window().maximize();
@@ -111,16 +114,23 @@ public void AddDispatchviaAdmin()
 
 @Test
 public void SearchByDateDispatch()
-{
-   driver.findElement(By.xpath("//a[normalize-space()='Line Haul']")).click();
+{   
+    WebElement chk = driver.findElement(By.xpath("//a[@href='javascript:void(0);' and text()='Line Haul ']"));
+	JavascriptExecutor js =(JavascriptExecutor)driver;
+	js.executeScript("argument[0].click()", chk);
+	
+  	// WebElement chk = driver.findElement(By.xpath("//a[@href='javascript:void(0);' and text()='Line Haul ']"));
+	    // ((JavascriptExecutor) driver).executeScript("arguments[0].click();", chk);
+	
+	
+	// driver.findElement(By.xpath("//a[normalize-space()='Line Haul']")).click();
    driver.findElement(By.xpath("//a[normalize-space()='Dispatch Form']")).click(); 	
    driver.findElement(By.id("daterange")).click();
    WebElement apply =  driver.findElement(By.id("daterange"));   apply.sendKeys("03/01/2024 - 03/30/2024");///////////////////////////////Dynamic data   apply.sendKeys(Keys.ENTER);
   
 }
 
-//GH
-
+//GH/
 
 
 	
@@ -164,5 +174,3 @@ public void SearchByDateDispatch()
 
 }
   
-  
-

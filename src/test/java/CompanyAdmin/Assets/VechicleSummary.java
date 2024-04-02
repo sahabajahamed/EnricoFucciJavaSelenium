c8ba3f7bf33dba8acff2afbf01b60f866a1145ba
pixtelation <abhishekmehta50@gmail.com>
2024-04-02 19:14:28 +0530

    Vehicle summary

--- src/test/java/CompanyAdmin/Assets/VechicleSummary.java
+++ src/test/java/CompanyAdmin/Assets/VechicleSummary.java
@@ -1,9 +1,80 @@
 package CompanyAdmin.Assets;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import DataFolder.Data;
+
 public class VechicleSummary {
-    public static void main(String[] args) {
-        System.out.println("Summary");
-        
-    }
+   
+
+	WebDriver driver;
+	String msg = "AVR has been successfully inserted";
+
+    static int RanTruck = Data.getRandomTruckNumber();
+    static int RanTruckYear = Data.getRandomTruckYear();
+
+@BeforeSuite	
+	public void OpenBrowser() 
+	{
+	// ChromeOptions options = new ChromeOptions();
+	// options.addArguments("--headless");
+	
+		// WebDriverManager.safaridriver().setup();
+		// driver = new SafariDriver();	
+
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	
+		driver.get("https://dev-test.groundmetrx.com/company/login");
+		driver.findElement(By.xpath("//input[@placeholder='Enter your username']")).sendKeys("DHL");
+		driver.findElement(By.xpath("//input[@placeholder='Enter password']")).sendKeys("DHL@123456");
+		driver.findElement(By.xpath("//button[@class='btn full-btn']")).sendKeys(Keys.RETURN);
+	}
+	
+@AfterSuite 
+  public void Closebrowser()
+  {
+	driver.quit();
+  }
+
     
+
+
+  @Test
+  public void AddAsets()
+  {
+    driver.findElement(By.xpath("//a[normalize-space()='Assets']")).click();
+    driver.findElement(By.xpath("//a[normalize-space()='Vehicle Summary']")).click();
+    driver.findElement(By.xpath("//a[@data-bs-target='#add-asset-modal']")).click();
+
+    driver.findElement(By.id("truck_number")).sendKeys(Integer.toString(RanTruck));
+    Select Entity = new Select(driver.findElement(By.id("entity_id")));
+    Entity.selectByVisibleText("DHL");
+    driver.findElement(By.id("make")).sendKeys(Data.RandomTruckMake);
+    driver.findElement(By.id("make")).sendKeys(Integer.toString(RanTruckYear));
+    driver.findElement(By.id("asset_type")).sendKeys(Data.RandTerminalStr); 
+    Select Department = new Select(driver.findElement(By.id("team")));
+    Department.selectByVisibleText("Line Haul");
+    Select Ownership = new Select(driver.findElement(By.id("ownership")));
+    Ownership.selectByVisibleText("Finance");
+
+
+  }
+
+
 }
--- src/test/java/DataFolder/Data.java
+++ src/test/java/DataFolder/Data.java
@@ -14,7 +14,7 @@ public class Data {
     public static String RandTerminalStr = RandomStringUtils.randomAlphabetic(4);
     public static String TerminalEmail = "@yopmail.com";
     public static String RandTerminalEmail = RandomStringUtils.randomAlphabetic(4);
-  
+    
     
     public static int getRandomTerminalNumber()
      {
@@ -22,15 +22,30 @@ public class Data {
         return rand.nextInt(10000); // Adjust range as needed
     }
     
-    
+       
     public static long getRandomPhoneNumber()
     {
         Random rand1 = new Random();
         return rand1.nextLong(1000000000l, 10000000000l); // Adjust range as needed
     }
-    //comm
+
+
+
+      // Truck module
+///////////////////////////////////////////////////////////
+    public static int getRandomTruckNumber()
+    {
+        Random rand3 = new Random();
+        return rand3.nextInt(10000); // Adjust range as needed
+    }
+
+    public static String RandomTruckMake = RandomStringUtils.randomAlphanumeric(8);
     
 
-   
+    public static int getRandomTruckYear()
+    {
+        Random rand3 = new Random();
+        return rand3.nextInt(2000,3000); // Adjust range as needed
+    }
     
 }
